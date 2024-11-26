54121ef744ee652a05389e54dfac072897ac9cd4
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-11-26 17:25:39 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- qemu/run.sh
+++ qemu/run.sh
@@ -10,18 +10,18 @@ eval ${COMMAND}
 
 sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} \
 ${QEMU_DIR}/deploy/usr/local/bin/qemu-system-aarch64 \
-   -machine type=virt,accel=kvm,virtualization=on \
-   -cpu max \
+   -machine type=virt,acpi=off,secure=off,mte=on,accel=kvm,virtualization=on,iommu=smmuv3,gic-version=max \
    -enable-kvm \
+   -cpu max,sme=off \
    -smp 4 \
    -m 8G \
    -d guest_errors \
    -nodefaults \
    -no-reboot \
    -nographic \
+   -serial mon:stdio \
    -kernel ${YOCTO_DIR}/xen-generic-armv8-xt \
    -append "dom0_mem=3G,max:3G loglvl=all guest_loglvl=all console=dtuart" \
-   -serial mon:stdio \
    -device guest-loader,addr=0x60000000,kernel=${YOCTO_DIR}/linux-dom0,bootargs="root=/dev/ram verbose loglevel=7 console=hvc0 earlyprintk=xen" \
    -device guest-loader,addr=0x52000000,initrd=${YOCTO_DIR}/rootfs.dom0.cpio.gz \
    -drive if=none,index=1,id=rootfs_domd,file=${YOCTO_DIR}/rootfs.domd.ext4 \
