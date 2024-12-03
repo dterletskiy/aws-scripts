949546af1f401e6f7ef0f3fc36db0eb8fe4bac13
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-03 14:45:55 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- common.sh
+++ common.sh
@@ -1,3 +1,14 @@
+# links:
+# https://github.com/eauger/qemu/commits/v9.0-nv-rfcv3/
+# https://git.kernel.org/pub/scm/linux/kernel/git/maz/arm-platforms.git/refs/heads
+# https://lists.gnu.org/archive/html/qemu-arm/2021-03/msg00853.html
+# https://patchew.org/QEMU/20230227163718.62003-1-miguel.luis@oracle.com/
+# https://lists.gnu.org/archive/html/qemu-devel/2024-02/msg02067.html
+# https://lore.kernel.org/all/20240209160039.677865-1-eric.auger@redhat.com/
+# https://patchwork.kernel.org/project/kvm/cover/20231222-kvm-arm64-sme-v2-0-da226cb180bb@kernel.org/
+
+
+
 ROOT_DIR=/home/ubuntu/workspace/
 YOCTO_DIR=${ROOT_DIR}/yocto/
 DUMP_DIR=${ROOT_DIR}/dump/
--- cpus.sh
+++ cpus.sh
@@ -1,10 +1,108 @@
 #!/usr/bin/env bash
 
+# Usage:
+#  Parameters:
+#     --cation: action to be performed
+#           - 'on' of 'enable' - enable cpu cores mentioned in '--cpus' parameter.
+#           - 'off' of 'disable' - enable cpu cores mentioned in '--cpus' parameter.
+#           - 'watch' - calculate core load in percentage during the time mentioned in the '--ms' paraneter
+#                       in second for the cores mentioned in '--cpus' parameter.
+#     --cpus: list of the cpus/cores separeted by ',' what will be effected by the action.
+#              It is also possible to set the cores range using '-'.
+#     --ms: time measurement in the second for actions: 'watch'
+# 
+# Examples:
+#     cpus.sh --action=watch --cpus=0-7 --ms=10
+#     sudo cpus.sh --action=off --cpus=1-3,6,7
+#     sudo cpus.sh --action=on --cpus=1-3
+
 
 
 declare -a CPUS
 
-parse_range( )
+# This function parses "grep 'cpu' /proc/stat" output and build dictionaty
+# where keys are the cores numbers mentioned in the firtst parameter and
+# values are the vectors of values for this core. 
+function __build_cpu_stat__( )
+{
+   local LOCAL_GREP_OUTPUT=${1}
+   declare -n LOCAL_CPU_DATA=${2}
+
+   # Processing each string
+   while IFS= read -r line; do
+      # Splitting string to array
+      read -a items <<< "$line"
+
+      # Test if string starts from 'cpu'
+      if [[ ${items[0]} =~ ^cpu[0-9]+$ ]]; then
+         # Gettinf core number
+         CORE=${items[0]#cpu}
+
+         # Removing first item ('cpuX') and storing array
+         CORE_DATA=("${items[@]:1}")
+
+         # Storing array to the didictionary
+         LOCAL_CPU_DATA[${CORE}]=${CORE_DATA[*]}
+      fi
+   done <<< "${LOCAL_GREP_OUTPUT}"
+}
+
+function __calc_core_load__( )
+{
+   declare -n LOCAL_START_CORE_DATA=${1}
+   declare -n LOCAL_STOP_CORE_DATA=${2}
+
+   START_CORE_LOAD_DATA=$(( LOCAL_START_CORE_DATA[0] + LOCAL_START_CORE_DATA[1] + LOCAL_START_CORE_DATA[2] ))
+   START_CORE_ALL_DATA=$(( START_CORE_LOAD_DATA + LOCAL_START_CORE_DATA[3] ))
+
+   STOP_CORE_LOAD_DATA=$(( LOCAL_STOP_CORE_DATA[0] + LOCAL_STOP_CORE_DATA[1] + LOCAL_STOP_CORE_DATA[2] ))
+   STOP_CORE_ALL_DATA=$(( STOP_CORE_LOAD_DATA + LOCAL_STOP_CORE_DATA[3] ))
+
+   CORE_LOAD_DELTA=$(( STOP_CORE_LOAD_DATA - START_CORE_LOAD_DATA ))
+   CORE_ALL_DELTA=$(( STOP_CORE_ALL_DATA - START_CORE_ALL_DATA ))
+
+   CORE_LOAD=$(( CORE_LOAD_DELTA * 100 / CORE_ALL_DELTA ))
+   echo ${CORE_LOAD}
+}
+
+function cpu_watch( )
+{
+   local LOCAL_MEASURE_SECONDS=${1}
+   declare -n LOCAL_CPUS=${2}
+
+   local START_TIME=$(date +%s.%N | cut -b1-14)
+   local START_METRIX=$(grep 'cpu' /proc/stat)
+   sleep ${LOCAL_MEASURE_SECONDS}
+   local STOP_METRIX=$(grep 'cpu' /proc/stat)
+   local STOP_TIME=$(date +%s.%N | cut -b1-14)
+
+   declare -A START_CPU_DATA
+   __build_cpu_stat__ "${START_METRIX}" START_CPU_DATA
+   declare -A STOP_CPU_DATA
+   __build_cpu_stat__ "${STOP_METRIX}" STOP_CPU_DATA
+
+   for CORE in ${LOCAL_CPUS[@]}; do
+      read -a START_CORE_DATA <<< "${START_CPU_DATA[${CORE}]}"
+      read -a STOP_CORE_DATA <<< "${STOP_CPU_DATA[${CORE}]}"
+
+      CORE_LOAD=$(__calc_core_load__ START_CORE_DATA STOP_CORE_DATA)
+      echo "Core ${CORE} load ${CORE_LOAD}%"
+   done
+}
+
+function cpu_on_off( )
+{
+   local LOCAL_ENABLE=${1}
+   declare -n LOCAL_CPUS=${2}
+
+   for CPU in ${LOCAL_CPUS[@]}; do
+      COMMAND="echo ${LOCAL_ENABLE} > /sys/devices/system/cpu/cpu${CPU}/online"
+      echo "${COMMAND}"
+      eval "${COMMAND}"
+   done
+}
+
+function parse_range( )
 {
    local INPUT_STRING="${1}"
    local -n RESULT_ARRAY=${2}
@@ -60,6 +158,11 @@ function validate_parameters( )
       parse_range "${CMD_CPUS}" CPUS
    fi
 
+   if [ -z ${CMD_MEASURE_SECONDS+x} ]; then
+      echo "'--ms' is not set => '1' will be used"
+      CMD_MEASURE_SECONDS=1
+   fi
+
    if [ -z ${CMD_DEBUG+x} ]; then
       echo "'--debug' is not defined"
       CMD_DEBUG=0
@@ -96,6 +199,16 @@ function parse_arguments( )
                exit 1
             fi
          ;;
+         --ms=*)
+            if [ -z ${CMD_MEASURE_SECONDS+x} ]; then
+               CMD_MEASURE_SECONDS="${option#*=}"
+               shift # past argument=value
+               echo "CMD_MEASURE_SECONDS: ${CMD_MEASURE_SECONDS}"
+            else
+               echo "'--ms' is already set to '${CMD_MEASURE_SECONDS}'"
+               exit 1
+            fi
+         ;;
          --debug)
             CMD_DEBUG=
             echo "CMD_DEBUG: defined"
@@ -114,14 +227,15 @@ function main( )
 {
    parse_arguments "$@"
 
-   local ENABLE=0
-
    case ${CMD_ACTION} in
       enable|on)
-         ENABLE=1
+         cpu_on_off 1 CPUS
       ;;
       disable|off)
-         ENABLE=0
+         cpu_on_off 0 CPUS
+      ;;
+      watch)
+         cpu_watch ${CMD_MEASURE_SECONDS} CPUS
       ;;
       *)
          echo "undefined CMD_ACTION: '${CMD_ACTION}'"
@@ -129,13 +243,10 @@ function main( )
       ;;
    esac
 
-   for CPU in ${CPUS[@]}; do
-      COMMAND="echo ${ENABLE} > /sys/devices/system/cpu/cpu${CPU}/online"
-      echo "${COMMAND}"
-      eval "${COMMAND}"
-   done
 }
 
 
 
 main "$@"
+
+
