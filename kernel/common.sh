d73ac61560abea54527d7f4fb7a8bd9ba203c858
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-06 10:15:53 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- kernel/common.sh
+++ kernel/common.sh
@@ -2,6 +2,7 @@ readonly SCRIPT_DIR_COMMON="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/nul
 source ${SCRIPT_DIR_COMMON}/../common.sh
 
 KERNEL_REMOTE="https://git.kernel.org/pub/scm/linux/kernel/git/maz/arm-platforms.git/"
-KERNEL_BRANCH="kvm-arm64/nv-6.11-sve-WIP"
+# KERNEL_BRANCH="kvm-arm64/nv-6.11-sve-WIP"
+KERNEL_BRANCH="kvm-arm64/nv-next"
 
 KERNEL_DIR=${ROOT_DIR}/kernel/
