4eaecde53c799b48a5e88a3db7e25cc2ad28a8de
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-10 15:39:36 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- qemu/common.sh
+++ qemu/common.sh
@@ -1,8 +1,11 @@
 readonly SCRIPT_DIR_COMMON="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source ${SCRIPT_DIR_COMMON}/../common.sh
 
-QEMU_REMOTE="https://github.com/eauger/qemu.git"
-QEMU_BRANCH="v9.0-nv-rfcv3"
+# QEMU_REMOTE="https://github.com/eauger/qemu.git"
+# QEMU_BRANCH="v9.0-nv-rfcv3"
+
+QEMU_REMOTE="https://github.com/dterletskiy/qemu_eauger.git"
+QEMU_BRANCH="v9.0-nv-rfcv3-ext"
 
 QEMU_DIR=${ROOT_DIR}/qemu/
 QEMU_SOURCE_DIR=${QEMU_DIR}/source/
