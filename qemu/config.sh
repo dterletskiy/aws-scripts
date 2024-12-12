48c5212f1f0be4817a0678f2defe0f55e90c88b2
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-12 21:02:31 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- qemu/config.sh
+++ qemu/config.sh
@@ -6,6 +6,7 @@ source ${SCRIPT_DIR}/common.sh
 CONFIG_OPTIONS=""
 CONFIG_OPTIONS+=" --target-list=aarch64-softmmu"
 CONFIG_OPTIONS+=" --enable-debug"
+CONFIG_OPTIONS+=" --disable-strip"
 CONFIG_OPTIONS+=" --enable-user"
 CONFIG_OPTIONS+=" --enable-slirp"
 CONFIG_OPTIONS+=" --enable-tools"
