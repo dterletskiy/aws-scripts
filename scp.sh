b4bc7175577a2a08a4a1202674e2b25a06657572
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-17 17:47:37 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- scp.sh
+++ scp.sh
@@ -1,6 +1,6 @@
 KEY="/home/dmytro_terletskyi/.tda/aws/epam/c8g.metal-24xl-2024.11.19.pem"
 REMOTE_USER=ubuntu
-REMOTE_IP=44.242.171.130
+REMOTE_IP=44.242.156.218
 REMOTE=${REMOTE_USER}@${REMOTE_IP}
 REMOTE_DIR="/home/ubuntu/workspace/yocto/"
 REMOTE_PATH=${REMOTE}:${REMOTE_DIR}
@@ -16,13 +16,12 @@ FILE=./qemu-image-minimal-generic-armv8-xt.rootfs-20241116080357.ext4
 scp -i ${KEY} -r ${FILE} ${REMOTE_PATH}/rootfs.domd.ext4
 FILE=./xen-generic-armv8-xt
 scp -i ${KEY} -r ${FILE} ${REMOTE_PATH}/xen-generic-armv8-xt
+FILE=./u-boot-generic-armv8-xt.bin
+scp -i ${KEY} -r ${FILE} ${REMOTE_PATH}/u-boot-generic-armv8-xt.bin
 
 cd /mnt/dev/docker/builder/epam/meta-xt-prod-qemu/build/yocto/build-domd/tmp/work/aarch64-poky-linux/xen/4.19.0+stable/git/xen/
 FILE=./xen-syms
 scp -i ${KEY} -r ${FILE} ${REMOTE_PATH}/xen-syms
-
-FILE=../xen.zip
-scp -i ${KEY} -r ${FILE} ${REMOTE_PATH}/
 
 
 
