69214f27c8060d2f9593ac8c415b4d4e460da9b7
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-19 15:11:41 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- kvmtool/run.sh
+++ kvmtool/run.sh
@@ -34,7 +34,7 @@ DOMD_ROOTFS=${YOCTO_DIR}/rootfs.domd.ext4
 
 COMMAND=""
 COMMAND+="sudo ${KVMTOOL_SOURCE_DIR}/lkvm run"
-COMMAND+=" -f ${XEN}"
+COMMAND+=" -f ${UBOOT}"
 # COMMAND+=" -k ${XEN}"
 # COMMAND+=" -p \"${XEN_CMD_LINE}\""
 COMMAND+=" -m 8G"
