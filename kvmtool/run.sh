7a675a777a4c5d0f3c5f3f0828d5d8c544b897d2
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-24 15:39:54 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- kvmtool/run.sh
+++ kvmtool/run.sh
@@ -36,7 +36,8 @@ DOMD_ROOTFS=${YOCTO_DIR}/rootfs.domd.ext4
 
 COMMAND=""
 COMMAND+="sudo ${KVMTOOL_SOURCE_DIR}/lkvm run"
-COMMAND+=" -k ${UBOOT}"
+# COMMAND+=" -k ${UBOOT}"
+COMMAND+=" -f /home/ubuntu/workspace/edk2/edk2-stable202411/source/Build/ArmVirtQemu-AARCH64/DEBUG_GCC5/FV/QEMU_EFI.fd"
 # COMMAND+=" -k ${XEN}"
 # COMMAND+=" -p \"${XEN_CMD_LINE}\""
 # COMMAND+=" -i ${DOM0_KERNEL}"
--- scp.sh
+++ scp.sh
@@ -1,12 +1,12 @@
 KEY="/home/dmytro_terletskyi/.tda/aws/epam/c8g.metal-24xl-2024.11.19.pem"
 REMOTE_USER=ubuntu
-REMOTE_IP=44.243.252.116
+REMOTE_IP=35.95.103.14
 REMOTE=${REMOTE_USER}@${REMOTE_IP}
 REMOTE_DIR="/home/ubuntu/workspace/yocto/"
 REMOTE_PATH=${REMOTE}:${REMOTE_DIR}
 
 cd /mnt/dev/docker/builder/epam/meta-xt-prod-qemu/build/yocto/build-dom0/tmp/deploy/images/generic-armv8-xt/
-FILE=core-image-thin-initramfs-generic-armv8-xt.rootfs-20241119024009.cpio.gz
+FILE=core-image-thin-initramfs-generic-armv8-xt.rootfs-20241222173931.cpio.gz
 scp -i ${KEY} -r ${FILE} ${REMOTE_PATH}/rootfs.dom0.cpio.gz
 FILE=Image--6.8.0-rc1+git0+6613476e22-r0-generic-armv8-xt-20241114105406.bin
 scp -i ${KEY} -r ${FILE} ${REMOTE_PATH}/linux-dom0
