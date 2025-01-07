fb58d3bf35c1b25fd3097e6f88f9b6374b58174a
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-01-07 13:12:49 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- edk2/build.sh
+++ edk2/build.sh
@@ -3,7 +3,7 @@
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source ${SCRIPT_DIR}/common.sh
 
-cd ${KVMTOOL_SOURCE_DIR}
+cd ${EDK2_SOURCE_DIR}
 
 source edksetup.sh
 
@@ -12,7 +12,7 @@ make -C BaseTools
 build -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtKvmTool.dsc
 build -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc
 
-cd ${KVMTOOL_SOURCE_DIR}/Build/ArmVirtQemu-AARCH64/DEBUG_GCC5/FV/
+cd ${EDK2_SOURCE_DIR}/Build/ArmVirtQemu-AARCH64/DEBUG_GCC5/FV/
 dd of="QEMU_EFI-pflash.raw" if="/dev/zero" bs=1M count=64
 dd of="QEMU_EFI-pflash.raw" if="QEMU_EFI.fd" conv=notrunc
 dd of="QEMU_VARS-pflash.raw" if="/dev/zero" bs=1M count=64
