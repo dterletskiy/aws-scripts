800e1a66dcbeff64e0085cd7009d7a59cd5d26a4
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-01-20 19:57:52 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- kernel/kexec.sh
+++ kernel/kexec.sh
@@ -11,5 +11,10 @@ KERNEL=/boot/vmlinuz
 INITRD=/boot/initrd.img
 CMDLINE="BOOT_IMAGE=/vmlinuz root=PARTUUID=021ed981-cb06-49a6-ade9-707e7abe39ab ro console=tty1 console=ttyS0 nvme_core.io_timeout=4294967295 kvm-arm.mode=nested panic=-1"
 
-sudo kexec -l ${KERNEL} --initrd=${INITRD} --command-line="${CMDLINE}"
-sudo kexec -e
+execute " \
+	sudo kexec -l ${KERNEL} --initrd=${INITRD} --command-line=\"${CMDLINE}\" \
+"
+
+execute " \
+	sudo kexec -e \
+"
