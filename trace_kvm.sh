1e4a14bdda1ed4a564908eb247932eec65cf0e06
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-01-22 19:35:22 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- trace_kvm.sh
+++ trace_kvm.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source ${SCRIPT_DIR}/common.sh
+
+readonly TIMESTAMP=$(date +'%Y.%m.%d_%H.%M.%S')
+
+
+
+clear
+
+
+
+KVM_DUMP_DIR=$(dump_dir)/kvm/${TIMESTAMP}/
+mkdir -p ${KVM_DUMP_DIR}
+
 sudo -i bash -c "echo 1 > /sys/kernel/debug/tracing/events/kvm/enable"
-sudo -i bash -c "cat /sys/kernel/debug/tracing/trace_pipe"
+sudo cat /sys/kernel/debug/tracing/trace_pipe | tee ${KVM_DUMP_DIR}/trace_pipe.log
