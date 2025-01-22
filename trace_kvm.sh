aa3f5b7e575f2f35e788e1d55b75fb36335b7cd2
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-01-22 18:43:33 +0200

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- trace_kvm.sh
+++ trace_kvm.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-sudo -i
-echo 1 > /sys/kernel/debug/tracing/events/kvm/enable
-cat /sys/kernel/debug/tracing/trace_pipe
+sudo -i bash -c "echo 1 > /sys/kernel/debug/tracing/events/kvm/enable"
+sudo -i bash -c "cat /sys/kernel/debug/tracing/trace_pipe"
