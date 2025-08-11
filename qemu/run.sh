d5b830a96407c8e9e5f82cfe92704ef740fc0427
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-08-11 13:21:01 +0300

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- qemu/run.sh
+++ qemu/run.sh
@@ -321,6 +321,7 @@ function build_params_common( )
 LD_LIBRARY_PATH+=":${QEMU_DIR}/lib/"
 LD_LIBRARY_PATH+=":${QEMU_DIR}/lib/x86_64-linux-gnu/"
 LD_LIBRARY_PATH+=":${QEMU_DIR}/lib/aarch64-linux-gnu/"
+print_info "LD_LIBRARY_PATH = '${LD_LIBRARY_PATH}'"
 COMMAND="export LD_LIBRARY_PATH"
 execute "${COMMAND}"
 
